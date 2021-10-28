f6a800a2f6e9798f168485509608268e74e4ce30
orinuguri <pentium07@daum.net>
2021-10-28 16:16:56 +0900

    NaverMovieReview

---
+++ practice/NaverMovieReviewList.py
@@ -0,0 +1,27 @@
+# NaverMovieReviewList.py
+# -> Naver 영화 (1개 선택) 리뷰 정보 수집(리뷰내용, 점수, 작성자이름, 날짜)
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://movie.naver.com/movie/bi/mi/pointWriteFormList.naver?code=206657&type=after&isActualPointWriteExecute=false&isMileageSubscriptionAlready=false&isMileageSubscriptionReject=false&page=1'
+result = requests.get(url)
+doc = BeautifulSoup(result.text, 'html.parser')
+
+review_list = doc.select('div.score_result > ul > li')
+
+for i, one in enumerate(review_list):
+    print('## REVIEW -> {} #################################################################'.format(i+1))
+
+    # 평점 정보 수집
+    score = one.select('div.star_score > em')[0].get_text()
+
+    # 리뷰 정보 수집
+    review = one.select('div.score_reple > p > span')[0].get_text()
+
+    print(':: REVIEW -> {}'.format(review))
+    print(':: SCORE -> {}'.format(score))
+
+
+
+
